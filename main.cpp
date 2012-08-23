delcypher/stpwrap2

commit 2c760bb533223bbb86c69f5741b066a954a51ce6

Fixed hang bug. This was caused by sending data (in this case a very large query ~76k) down the pipe with no program listening on the otherside. Presumably the kernel buffer was filled and blocked waiting for a program to consume the read end of the pipe. However there was no read end so the wrapper would hang.
To fix this STP is now started before parsing the input file so that as we send down the pipe STP consumes from the read end of the pipe.

main.cpp
@@ -108,12 +108,14 @@ int cpfd[2];
 void parseArgs(int argc, char* argv[]);
 void setupPipes();
 
-
+pid_t pid;
 
 
 void printSMTLIBResponce();
 
 void runSTP();
+
+void waitForSTP();
 
 void exitUnknown();
 
@@ -123,9 +125,11 @@ int main(int argc, char* argv[])
 
 	setupPipes();
 
+	runSTP();
+
 	SMTLIBInput::parseInput();
 
-	runSTP();
+	waitForSTP();
 
 	SMTLIBOutput::parseOutput();
 
@@ -319,7 +323,7 @@ void runSTP()
 {
 	fflush(stdout);
 	fflush(stdin);
-	pid_t pid = fork();
+	pid = fork();
 
 	if(pid==-1)
 	{
@@ -332,10 +336,14 @@ void runSTP()
 	{
 		//child
 
-
-		/* pcfd[1] (write end) (a.k.a ilout) does not need closing as it has
-		 * already been closed before fork
+		/* Close write end of parent->child.
+		 * EOF will never be sent if we forget to do this!
 		 */
+		if(close(pcfd[1]) == -1)
+		{
+			perror("close (child):");
+			exit(1);
+		}
 
 
 		//close read end of child->parent
@@ -365,33 +373,24 @@ void runSTP()
 		}
 
 		//execute STP
-		if(solutions.size()== 0)
+
+
+		/* We always ask for a counter example. We do this because
+		 * we don't know yet if we need to ask for values because
+		 * the input SMTLIBv2 file has not been parsed.
+		 */
+		if(execlp(STP,STP,"--SMTLIB2", "-p", (char*) NULL)== -1)
 		{
-			//we don't need to ask for a counter example
-			if(execlp(STP,STP,"--SMTLIB2", (char*) NULL)== -1)
-			{
-				cerr << "Failed to execute STP (" << STP << ")" << endl;
-				perror("execlp:");
-				exit(1);
-			}
+			cerr << "Failed to execute STP (" << STP << ")" << endl;
+			perror("execlp:");
+			exit(1);
 		}
-		else
-		{
-			//We need to ask for a counter example
-			if(execlp(STP,STP,"--SMTLIB2", "-p", (char*) NULL)== -1)
-			{
-				cerr << "Failed to execute STP (" << STP << ")" << endl;
-				perror("execlp:");
-				exit(1);
-			}
-		}
+
 
 	}
 	else
 	{
 		//parent
-
-		int status=0;
 
 
 		//close read end of parent->child
@@ -408,24 +407,31 @@ void runSTP()
 			exit(1);
 		}
 
-		//Wait for child to complete
-		if(waitpid(pid,&status,0)==-1)
-		{
-			perror("waitpid:");
-			exit(1);
-		}
+		//now return. We'll wait for child once input parsing has finished
 
-		if(WIFEXITED(status) && WEXITSTATUS(status)==0)
-			return;
-		else
-		{
-			cerr << "STP did not return correctly!" << endl;
-			exitUnknown();
-		}
 	}
 }
 
+void waitForSTP()
+{
+	int status;
+	//Wait for child to complete
+	if(waitpid(pid,&status,0)==-1)
+	{
+		perror("waitpid:");
+		exit(1);
+	}
 
+	if(WIFEXITED(status) && WEXITSTATUS(status)==0)
+	{
+		return;
+	}
+	else
+	{
+		cerr << "STP did not return correctly!" << endl;
+		exitUnknown();
+	}
+}
 
 void exitUnknown()
 {

commit 4f6cd0592620b4e379ca12e9d58962bd5f361249

Fixed bug introduced by commit 2c760bb533223bbb86c69f5741b066a954a51ce6.
STP now always generates counter examples, even if we don't want them. We now skip over the counter example
if we don't want array values.

main.cpp
@@ -447,11 +447,23 @@ void SMTLIBOutput::parseOutput()
 	{
 		//We don't need to get array values
 
-		//flex should send these to stdout so we don't need to.
-		if(t!= T_SAT && t!= T_UNSAT && t!= T_UNKNOWN)
+		/* Because we told STP we want a counter example. We'll
+		 * still see the arrays values. We'll just skip past them
+		 * until we see what we want.
+		 */
+
+		while(t!= T_SAT && t!= T_UNSAT && t!= T_UNKNOWN)
 		{
-			cerr << "Error: STP did not respond with sat|unsat|unknown." << endl;
+			t=static_cast<OutputToken>(ollex());//grab next token
+			if(t==T_EOF)
+			{
+				cerr << "Error: STP did not respond with sat|unsat|unknown." << endl;
+			}
 		}
+
+		/* When we hit T_SAT | T_UNSAT | T_UNKNOWN token they'll be sent to
+		 * stdout by flex so we don't need to do anything else.
+		 */
 	}
 	else
 	{